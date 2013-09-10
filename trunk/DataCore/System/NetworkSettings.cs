bb3e621541cd48fb105f62e8b6d67ca79a3d8400
roger.castaldo <roger.castaldo@e005b064-1ef6-b701-f440-c3a5eb7df489>
2013-09-10 17:07:46 +0000

    added some code to allow for a more thorough checking if the interface is live by checking for a subinterface and then if it is, attempting to check the parent interface.

--- trunk/DataCore/System/NetworkSettings.cs
+++ trunk/DataCore/System/NetworkSettings.cs
@@ -34,6 +34,7 @@ namespace Org.Reddragonit.FreeSwitchConfig.DataCore.System
 
         private static readonly Regex _regSearch = new Regex("([\\d\\.]+)%\\s+packet\\s+loss", RegexOptions.Compiled | RegexOptions.ECMAScript);
         private static readonly Regex _regIface = new Regex("^(lo|((eth|wlan|vboxnet|pan|vmnet|ppp|venet)\\d+(:\\d+)?))\\s+",RegexOptions.Compiled|RegexOptions.ECMAScript);
+        private static readonly Regex _regSubInterface = new Regex("^((lo|((eth|wlan|vboxnet|pan|vmnet|ppp|venet)\\d+):\\d+$", RegexOptions.ECMAScript | RegexOptions.Compiled);
         
         public const string IFCONFIGCommand = "/sbin/ifconfig";
         public const string ARPCommand = "/sbin/arp";
@@ -391,7 +392,9 @@ namespace Org.Reddragonit.FreeSwitchConfig.DataCore.System
                 string results = sr.ReadToEnd();
                 sr.Close();
                 return results.Trim().ToUpper() == "UP" || results.Trim().ToUpper() == "UNKOWN";
-            } 
+            }
+            else if (_regSubInterface.IsMatch(interfaceName))
+                return IsInterfaceLive(_regSubInterface.Match(interfaceName).Groups[1].Value);
             return true;
         }
 
