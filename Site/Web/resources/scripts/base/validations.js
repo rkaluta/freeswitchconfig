1436a3c9857ca236c817dba784bb499d2228d6dd
roger.castaldo <roger.castaldo@e005b064-1ef6-b701-f440-c3a5eb7df489>
2013-09-11 16:12:41 +0000

    corrected missing code for adding error objects including the required style sheet/style.

--- Site/Web/resources/scripts/base/validations.js
+++ Site/Web/resources/scripts/base/validations.js
@@ -1,7 +1,10 @@
 CreateNameSpace('FreeswitchConfig.Site.Validation');
 
 FreeswitchConfig.Site.Validation = $.extend(FreeswitchConfig.Site.Validation, {
-    IsValidNPANXXValue:function(value) {
+    ProduceErrorObject: function(value) {
+        return FreeswitchConfig.Site.Skin.span.Create({ Content: value, Class: 'Error' });
+    },
+    IsValidNPANXXValue: function(value) {
         var reg = new RegExp('^((\\d|N|Z|X|(\\[\\d+-\\d+\\])|(\\[(\\d+,)+\\d+\\])|\\.)+\\|?(\\d|N|Z|X|(\\[\\d+-\\d+\\])|(\\[(\\d+,)+\\d+\\])|\\.)+)$');
         if (value.indexOf('\n') >= 0) {
             var tmp = value.split('\n');
@@ -15,43 +18,43 @@ FreeswitchConfig.Site.Validation = $.extend(FreeswitchConfig.Site.Validation, {
         }
         return true;
     },
-    IsValidIPAddress:function(value) {
+    IsValidIPAddress: function(value) {
         var reg = new RegExp('^(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))$');
         return reg.test(value);
     },
-    IsValidDialableNumber:function(value) {
+    IsValidDialableNumber: function(value) {
         var reg = new RegExp('^(\\d{7,25})$');
         return reg.test(value);
     },
-    IsValidDomainName:function(value) {
+    IsValidDomainName: function(value) {
         var reg = new RegExp('^([a-zA-Z0-9]([a-zA-Z0-9\\-]{0,61}[a-zA-Z0-9])?\\.)*[a-zA-Z0-9]([a-zA-Z0-9\\-]{0,61}[a-zA-Z0-9])?$');
         return reg.test(value);
     },
-    IsValidPositiveInteger:function(value) {
+    IsValidPositiveInteger: function(value) {
         var reg = new RegExp('^(\\d+)$');
         return reg.test(value);
     },
-    IsValidInteger:function(value){
+    IsValidInteger: function(value) {
         var reg = new RegExp('^(-)?(\\d+)$');
         return reg.test(value);
     },
-    IsValidMACAddress:function(value) {
+    IsValidMACAddress: function(value) {
         var reg = new RegExp('^([0-9A-F]{2}(:|-)?){5}[0-9A-F]{2}$');
         return reg.test(value);
     },
-    IsValidEmail:function(email) {
+    IsValidEmail: function(email) {
         var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
         return reg.test(email);
     },
-    IsValidVersionNumber:function(value) {
+    IsValidVersionNumber: function(value) {
         var reg = new RegExp('^(\\d+\\.)*\\d+$');
         return reg.test(value);
     },
-    ValidateEmailField:function(field){
+    ValidateEmailField: function(field) {
         field = $(field);
-        if (field.val() != '' && ! this.IsValidEmail(field.val())) {
+        if (field.val() != '' && !this.IsValidEmail(field.val())) {
             if (field.next().length == 0 || $(field.next()).html() != '*') {
-                field.after(ProduceErrorObject('*'));
+                field.after(FreeswitchConfig.Site.Validation.ProduceErrorObject('*'));
             }
             return false;
         } else if (field.next().length > 0 && $(field.next()).html() == '*') {
@@ -59,23 +62,11 @@ FreeswitchConfig.Site.Validation = $.extend(FreeswitchConfig.Site.Validation, {
         }
         return true;
     },
-    ValidateRequiredField:function(field) {
+    ValidateRequiredField: function(field) {
         field = $(field);
         if (field.val() == '') {
-            if (field.next().length==0 || $(field.next()).html() != '*') {
-                field.after(ProduceErrorObject('*'));
-            }
-            return false;
-        } else if (field.next().length>0 && $(field.next()).html() == '*') {
-            $(field.next()).remove();
-        }
-        return true;
-    },
-    ValidateFieldLength: function(field,length) {
-        field = $(field);
-        if (field.val().length>length) {
             if (field.next().length == 0 || $(field.next()).html() != '*') {
-                field.after(ProduceErrorObject('*'));
+                field.after(FreeswitchConfig.Site.Validation.ProduceErrorObject('*'));
             }
             return false;
         } else if (field.next().length > 0 && $(field.next()).html() == '*') {
@@ -83,11 +74,23 @@ FreeswitchConfig.Site.Validation = $.extend(FreeswitchConfig.Site.Validation, {
         }
         return true;
     },
-    ValidateVersionNumberField:function(field) {
+    ValidateFieldLength: function(field, length) {
+        field = $(field);
+        if (field.val().length > length) {
+            if (field.next().length == 0 || $(field.next()).html() != '*') {
+                field.after(FreeswitchConfig.Site.Validation.ProduceErrorObject('*'));
+            }
+            return false;
+        } else if (field.next().length > 0 && $(field.next()).html() == '*') {
+            $(field.next()).remove();
+        }
+        return true;
+    },
+    ValidateVersionNumberField: function(field) {
         field = $(field);
         if (field.val() != '' && !this.IsValidVersionNumber(field.val())) {
             if (field.next().length == 0 || $(field.next()).html() != '*') {
-                field.after(ProduceErrorObject('*'));
+                field.after(FreeswitchConfig.Site.Validation.ProduceErrorObject('*'));
             }
             return false;
         } else if (field.next().length > 0 && $(field.next()).html() == '*') {
@@ -95,11 +98,11 @@ FreeswitchConfig.Site.Validation = $.extend(FreeswitchConfig.Site.Validation, {
         }
         return true;
     },
-    ValidatePositiveIntegerField:function(field) {
+    ValidatePositiveIntegerField: function(field) {
         field = $(field);
         if (field.val() != '' && !this.IsValidPositiveInteger(field.val())) {
             if (field.next().length == 0 || $(field.next()).html() != '*') {
-                field.after(ProduceErrorObject('*'));
+                field.after(FreeswitchConfig.Site.Validation.ProduceErrorObject('*'));
             }
             return false;
         } else if (field.next().length > 0 && $(field.next()).html() == '*') {
@@ -107,11 +110,11 @@ FreeswitchConfig.Site.Validation = $.extend(FreeswitchConfig.Site.Validation, {
         }
         return true;
     },
-    ValidateDialableNumberField:function(field) {
+    ValidateDialableNumberField: function(field) {
         field = $(field);
         if (field.val() != '' && !this.IsValidDialableNumber(field.val())) {
             if (field.next().length == 0 || $(field.next()).html() != '*') {
-                field.after(ProduceErrorObject('*'));
+                field.after(FreeswitchConfig.Site.Validation.ProduceErrorObject('*'));
             }
             return false;
         } else if (field.next().length > 0 && $(field.next()).html() == '*') {
@@ -119,23 +122,23 @@ FreeswitchConfig.Site.Validation = $.extend(FreeswitchConfig.Site.Validation, {
         }
         return true;
     },
-    ValidateIntegerInRange:function(field,min,max){
+    ValidateIntegerInRange: function(field, min, max) {
         field = $(field);
         if (field.val() != '' && !this.IsValidInteger(field.val())) {
             if (field.next().length == 0 || $(field.next()).html() != '*') {
-                field.after(ProduceErrorObject('*'));
+                field.after(FreeswitchConfig.Site.Validation.ProduceErrorObject('*'));
             }
             return false;
         } else if (field.val() != '' && this.IsValidInteger(field.val())) {
-            var val = 1*field.val();
-            min = (min==null ? val : min);
+            var val = 1 * field.val();
+            min = (min == null ? val : min);
             max = (max == undefined ? val : (max == null ? val : max));
-            if (val<min || val>max){
+            if (val < min || val > max) {
                 if (field.next().length == 0 || $(field.next()).html() != '*') {
-                    field.after(ProduceErrorObject('*'));
+                    field.after(FreeswitchConfig.Site.Validation.ProduceErrorObject('*'));
                 }
-                return false;    
-            }else if (field.next().length > 0 && $(field.next()).html() == '*') {
+                return false;
+            } else if (field.next().length > 0 && $(field.next()).html() == '*') {
                 $(field.next()).remove();
             }
         } else if (field.next().length > 0 && $(field.next()).html() == '*') {
@@ -143,11 +146,11 @@ FreeswitchConfig.Site.Validation = $.extend(FreeswitchConfig.Site.Validation, {
         }
         return true;
     },
-    ValidateNPANXXField:function(field) {
+    ValidateNPANXXField: function(field) {
         field = $(field);
         if (field.val() != '' && !this.IsValidNPANXXValue(field.val())) {
             if (field.next().length == 0 || $(field.next()).html() != '*') {
-                field.after(ProduceErrorObject('*'));
+                field.after(FreeswitchConfig.Site.Validation.ProduceErrorObject('*'));
             }
             return false;
         } else if (field.next().length > 0 && $(field.next()).html() == '*') {
@@ -155,11 +158,11 @@ FreeswitchConfig.Site.Validation = $.extend(FreeswitchConfig.Site.Validation, {
         }
         return true;
     },
-    ValidateIPAddressField:function(field) {
+    ValidateIPAddressField: function(field) {
         field = $(field);
         if (field.val() != '' && !this.IsValidIPAddress(field.val())) {
             if (field.next().length == 0 || $(field.next()).html() != '*') {
-                field.after(ProduceErrorObject('*'));
+                field.after(FreeswitchConfig.Site.Validation.ProduceErrorObject('*'));
             }
             return false;
         } else if (field.next().length > 0 && $(field.next()).html() == '*') {
@@ -167,11 +170,11 @@ FreeswitchConfig.Site.Validation = $.extend(FreeswitchConfig.Site.Validation, {
         }
         return true;
     },
-    ValidateMACAddressField:function(field) {
+    ValidateMACAddressField: function(field) {
         field = $(field);
         if (field.val() != '' && !this.IsValidMACAddress(field.val())) {
             if (field.next().length == 0 || $(field.next()).html() != '*') {
-                field.after(ProduceErrorObject('*'));
+                field.after(FreeswitchConfig.Site.Validation.ProduceErrorObject('*'));
             }
             return false;
         } else if (field.next().length > 0 && $(field.next()).html() == '*') {
@@ -179,11 +182,11 @@ FreeswitchConfig.Site.Validation = $.extend(FreeswitchConfig.Site.Validation, {
         }
         return true;
     },
-    ValidateDomainNameField:function(field) {
+    ValidateDomainNameField: function(field) {
         field = $(field);
         if (field.val() != '' && !this.IsValidDomainName(field.val())) {
             if (field.next().length == 0 || $(field.next()).html() != '*') {
-                field.after(ProduceErrorObject('*'));
+                field.after(FreeswitchConfig.Site.Validation.ProduceErrorObject('*'));
             }
             return false;
         } else if (field.next().length > 0 && $(field.next()).html() == '*') {
